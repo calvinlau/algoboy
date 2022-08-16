5e1a9d9fdb6ebf308c7e90e8a2c8d998e4c3feb4
祖冲 <zuchong.lk@antfin.com>
2022-08-16 11:47:24 +0800

    map&heap

--- leetcode/src/list/LinkedListCycle.java
+++ leetcode/src/list/LinkedListCycle.java
@@ -1,3 +1,4 @@
+package list;
 
 /**
  * Given a linked list, determine if it has a cycle in it.
--- leetcode/src/list/LinkedListCycleII.java
+++ leetcode/src/list/LinkedListCycleII.java
@@ -1,3 +1,5 @@
+package list;
+
 /**
  * Given a linked list, return the node where the cycle begins. If there is no cycle, return null.
  * 
